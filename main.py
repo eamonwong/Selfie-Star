1a338f02fa7e58349ec130c9065e413e52c2b113
eamon01 <75913022+eamonwong@users.noreply.github.com>
2021-06-23 11:20:05 +0100

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 smile_cascade = cv2.CascadeClassifier('haarcascade_smile.xml')
 while True:
     _, frame = cap.read()
-    original_frame = frame.copy
+    original_frame = frame.copy()
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     face = face_cascade.detectMultiScale(gray, 1.3, 5)
     for x, y, w, h in face:
